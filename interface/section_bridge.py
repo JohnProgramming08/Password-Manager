65eac474a057c7b93e6fb772fed1abeb421a7f4e
Dylan Scully <dylan08code@gmail.com>
2026-08-08 17:02:17 +0100

    test: add test coverage for bridge classes

--- interface/section_bridge.py
+++ interface/section_bridge.py
@@ -4,16 +4,17 @@ from services import Section
 class SectionBridge:
     def __init__(self, args):
         self.args = args
-        self.determine_function()
+        self.command = self.determine_function()
 
     # Determine which function to call based on the command
-    def determine_function(self):
+    def determine_function(self) -> str | None:
         args = self.args
 
-        # Create section [section_name]
+        # Section create [section_name]
         if (
             hasattr(args, "section")
             and hasattr(args, "create")
             and hasattr(args, "section_name")
         ):
-            Section.add_section(self.args.section_name)
+            Section.add_section(args.section_name)
+            return "create"
---
+++ tests/test_field_bridge.py
@@ -0,0 +1,39 @@
+from types import SimpleNamespace
+from interface import FieldBridge
+
+
+def test_none():
+    args1 = SimpleNamespace(field="activated", set="activated")
+    field_bridge1 = FieldBridge(args1)
+    assert field_bridge1.command is None
+
+    args2 = SimpleNamespace()
+    field_bridge1 = FieldBridge(args2)
+    assert field_bridge1.command is None
+
+
+def test_set(monkeypatch, filled_config_file):
+    monkeypatch.setattr("builtins.input", lambda _: "test")
+
+    args = SimpleNamespace(
+        field="activated",
+        set="activated",
+        section_name="filled",
+        field_name="sigma",
+        value="female",
+    )
+    field_bridge = FieldBridge(args)
+    assert field_bridge.command == "set"
+
+
+def test_get(monkeypatch, filled_config_file):
+    monkeypatch.setattr("builtins.input", lambda _: "test")
+
+    args = SimpleNamespace(
+        field="activated",
+        get="activated",
+        section_name="filled",
+        field_name="cant",
+    )
+    field_bridge = FieldBridge(args)
+    assert field_bridge.command == "get"
