57c874afc00fb24873a707fccd0e7f9d2033e70b
Dylan Scully <dylan08code@gmail.com>
2026-08-14 23:53:14 +0100

    feat: remove database as not needed

--- api/__init__.py
+++ api/__init__.py
@@ -8,8 +8,6 @@ from .routes import register_blueprints
 def create_app(config_overlay=None):
     load_dotenv()
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("database_uri")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Default configuration
     app.config.update(DEBUG=True, SECRET_KEY=os.getenv("secret_key"))
@@ -17,10 +15,6 @@ def create_app(config_overlay=None):
     # Apply test-specific overrides if they exist
     if config_overlay:
         app.config.update(config_overlay)
-
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
 
     register_blueprints(app)
 
--- api/database/create_db.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
